bb5da2c113c9994d98c5e724951a556faddcde11
Flo-fllt <florian.follet@epfl.ch>
2025-04-14 19:23:54 +0200

    Entrainement du modele mais pas encore test ni meme valid

--- affichage.py
+++ affichage.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from streamlit_ketcher import st_ketcher
 
-
 with st.expander("Draw Molecule"):
     molecule = st.text_input("**Smiles**") 
     ketcher_smiles= st_ketcher(molecule,height=600)
-
-
-
