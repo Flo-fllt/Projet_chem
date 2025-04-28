ffec38b9a8ea0d75d4fa7f71af4dab13f3dd8575
JacquesGrandjean <jacques.grandjean@epfl.ch>
2025-04-28 10:54:15 +0200

    Mise a jour des fonctions

--- Package_retrosynth/Retrosynthese/affichage.py
+++ Package_retrosynth/Retrosynthese/affichage.py
@@ -12,10 +12,12 @@ from rdkit.Chem import Draw
 from rdkit.Chem.Draw import rdMolDraw2D
 from PIL import Image
 from io import BytesIO
+import base64
+
 
 # --- High quality molecule drawing ---
-def mol_to_high_quality_image(mol, size=(300, 300)):
-    drawer = rdMolDraw2D.MolDraw2DCairo(size[0], size[1])
+def mol_to_high_quality_image(mol, size=(800, 800)):
+    drawer = rdMolDraw2D.MolDraw2DCairo(*size)
     opts = drawer.drawOptions()
     opts.bondLineWidth = 2.0
     drawer.DrawMolecule(mol)
@@ -23,7 +25,20 @@ def mol_to_high_quality_image(mol, size=(300, 300)):
     png = drawer.GetDrawingText()
     return Image.open(BytesIO(png))
 
-# --- Functions ---
+# --- Display scaled image (visually smaller but full resolution) ---
+def st_scaled_image(image, width_display_px=200):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    html = f"""
+    <div style="display:inline-block;">
+        <img src="data:image/png;base64,{img_str}" style="width:{width_display_px}px; height:auto;" />
+    </div>
+    """
+    st.markdown(html, unsafe_allow_html=True)
+
+
+# --- Helper functions ---
 def smiles_to_fingerprint(smiles, radius=2, n_bits=2048):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
@@ -43,18 +58,18 @@ def apply_template(template_smarts, smiles_input):
         product_smiles = []
         for prod_set in products:
             prod_list = [Chem.MolToSmiles(p) for p in prod_set if p is not None]
-            product_smiles.append(prod_list)
+            if prod_list:
+                product_smiles.append(prod_list)
         return product_smiles
     except:
         return []
 
 def predict_topk_templates(smiles_input, topk=50):
-    fingerprint = smiles_to_fingerprint(smiles_input)
-    fingerprint = fingerprint.reshape(1, -1)
+    fingerprint = smiles_to_fingerprint(smiles_input).reshape(1, -1)
     fingerprint_scaled = scaler.transform(fingerprint)
     probs = model.predict_proba(fingerprint_scaled)[0]
     topk_indices = np.argsort(probs)[::-1][:topk]
-    topk_template_hashes = model.classes_[topk_indices]
+    topk_template_hashes = label_encoder.inverse_transform(model.classes_[topk_indices])
     topk_probs = probs[topk_indices]
 
     predictions = []
@@ -63,7 +78,4 @@ def predict_topk_templates(smiles_input, topk=50):
         if not row.empty:
             retro_template = row.iloc[0]['RetroTemplate']
             predictions.append((template_hash, retro_template, prob))
-        else:
-            predictions.append((template_hash, None, prob))
     return predictions
-
